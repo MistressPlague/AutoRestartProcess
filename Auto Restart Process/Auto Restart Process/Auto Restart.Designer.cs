5025866bb1a4096be542a0b51c00f6dfb350ef51
Plague <36628963+OFWModz@users.noreply.github.com>
2021-03-23 13:01:50 +0000

    Source Code

---
+++ Auto Restart Process/Auto Restart Process/Auto Restart.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Auto_Restart_Process
+{
+    partial class AutoRestartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoRestartForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RestartWorker = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.FilePopup = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LogBox = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox1.Location = new System.Drawing.Point(85, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "D://Visual Studio Projects//VRCAntiCrash Bot//VRCAntiCrash Bot GUI//bin//Debug//V" +
+    "RCAntiCrash Bot GUI.exe";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(10, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Auto Restart";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(363, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kill All Matches";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Magenta;
+            this.numericUpDown1.Location = new System.Drawing.Point(54, 37);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Interval:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(10, 67);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Run On Startup";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Maintain This:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // RestartWorker
+            // 
+            this.RestartWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.RestartWorker_DoWork);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(85, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FilePopup
+            // 
+            this.FilePopup.RestoreDirectory = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Arguments:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox2.Location = new System.Drawing.Point(85, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(371, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // LogBox
+            // 
+            this.LogBox.BackColor = System.Drawing.Color.Black;
+            this.LogBox.ForeColor = System.Drawing.Color.Magenta;
+            this.LogBox.Location = new System.Drawing.Point(10, 138);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogBox.Size = new System.Drawing.Size(584, 133);
+            this.LogBox.TabIndex = 12;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(9, 101);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(116, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Create No Window";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(129, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 127);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Start In Window State:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Normal",
+            "Hidden",
+            "Minimized",
+            "Maximized"});
+            this.comboBox1.Location = new System.Drawing.Point(251, 99);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // AutoRestartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(607, 283);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.ForeColor = System.Drawing.Color.Magenta;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AutoRestartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Restart Process";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker RestartWorker;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog FilePopup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox LogBox;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Auto Restart Process/Auto Restart Process/Auto Restart.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+using Libraries;
+
+namespace Auto_Restart_Process
+{
+    public partial class AutoRestartForm : Form
+    {
+        public AutoRestartForm()
+        {
+            InitializeComponent();
+
+            Instance = this;
+        }
+
+        public static AutoRestartForm Instance;
+
+        public Configuration Config = new Configuration();
+
+        public class Configuration
+        {
+            public bool IsAutoRestarting
+            {
+                get => Instance.checkBox1.Checked;
+                set => Instance.checkBox1.Checked = value;
+            }
+
+            public decimal Interval
+            {
+                get => Instance.numericUpDown1.Value;
+                set => Instance.numericUpDown1.Value = value;
+            }
+
+            public bool RunOnStartup
+            {
+                get => Instance.checkBox2.Checked;
+                set => Instance.checkBox2.Checked = value;
+            }
+
+            public string MaintainThis
+            {
+                get => Instance.textBox1.Text;
+                set => Instance.textBox1.Text = value;
+            }
+
+            public string Arguments
+            {
+                get => Instance.textBox2.Text;
+                set => Instance.textBox2.Text = value;
+            }
+
+            public bool CreateNoWindow
+            {
+                get => Instance.checkBox3.Checked;
+                set => Instance.checkBox3.Checked = value;
+            }
+
+            public int WindowStartState
+            {
+                get => Instance.comboBox1.SelectedIndex;
+                set => Instance.comboBox1.SelectedIndex = value;
+            }
+        }
+
+        public static bool IsAdmin = false;
+
+        public bool IsUserAdministrator()
+        {
+            WindowsIdentity user = null;
+            try
+            {
+                //get the currently logged in user
+                user = WindowsIdentity.GetCurrent();
+
+                IsAdmin = new WindowsPrincipal(user).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                IsAdmin = false;
+            }
+            catch (Exception)
+            {
+                IsAdmin = false;
+            }
+            finally
+            {
+                if (user != null)
+                {
+                    user.Dispose();
+                }
+            }
+            return IsAdmin;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                RestartWorker.RunWorkerAsync();
+            }
+
+            JsonConfig.SaveConfig(Config);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Proc != null && !Proc.HasExited)
+                {
+                    Proc.Kill();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            JsonConfig.SaveConfig(Config);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IsUserAdministrator();
+
+            JsonConfig.LoadConfig(ref Config);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            JsonConfig.SaveConfig(Config);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkBox2.Checked)
+            {
+                rk.SetValue(Application.ExecutablePath, "\"" + Application.ExecutablePath + "\"");
+            }
+            else
+            {
+                rk.DeleteValue(Application.ExecutablePath, false);
+            }
+
+            JsonConfig.SaveConfig(Config);
+        }
+
+        public void Log(string text)
+        {
+            LogBox.AppendText("[" + DateTime.Now.ToString("hh:MM:ss tt") + "] " + text + "\r\n");
+        }
+
+        private Stopwatch TimePassed = new Stopwatch();
+
+        private Process Proc;
+
+        private void RestartWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            Log("RestartWorker Init!");
+
+            if (!TimePassed.IsRunning)
+            {
+                TimePassed.Start();
+            }
+
+            try
+            {
+                while (checkBox1.Checked)
+                {
+                    if (TimePassed.ElapsedMilliseconds >= (long)numericUpDown1.Value)
+                    {
+                        Log("Restarting!");
+
+                        ProcessStartInfo Info = new ProcessStartInfo
+                        {
+                            FileName = textBox1.Text,
+                            WorkingDirectory = Path.GetDirectoryName(textBox1.Text) ?? Environment.CurrentDirectory,
+                            Arguments = textBox2.Text.Replace("%APPDIR%", Path.GetDirectoryName(textBox1.Text) ?? Environment.CurrentDirectory).Replace("%TIME%", DateTime.Now.ToString("dd MM ss tt")),
+                            CreateNoWindow = checkBox3.Checked,
+                            WindowStyle = (ProcessWindowStyle)comboBox1.SelectedIndex
+                        };
+
+                        Proc = Process.Start(Info);
+
+                        Log("Process Started!");
+
+                        Proc?.WaitForExit();
+
+                        Log("Process Died" + (checkBox1.Checked ? " - Restarting Soon" : "") + "!");
+
+                        TimePassed.Restart();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Exception: " + ex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Result = FilePopup.ShowDialog();
+
+            if (Result == DialogResult.OK)
+            {
+                textBox1.Text = FilePopup.FileName.Replace("\\", "//");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            JsonConfig.SaveConfig(Config);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            JsonConfig.SaveConfig(Config);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            JsonConfig.SaveConfig(Config);
+        }
+    }
+}
